7058b3eb6830703ab40100c87216f3f308da7098
Viven Puthenpurayil <vivenputhenpurayil@gmail.com>
2018-09-20 20:42:37 -0400

    Central

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Central.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Central.java
@@ -1,8 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.bosch.BNO055IMUImpl;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import java.util.Arrays;
 
 public abstract class Central extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    //--------------------------------ENCODERS-------------------------
+    public static final double COUNTS_PER_MOTOR_NEVEREST = 1680;
+    public static final double COUNTS_PER_MOTOR_TETRIX = 1440;
+    public static final double DRIVE_GEAR_REDUCTION = 1.0;
+    public static final double WHEEL_DIAMETER_INCHES = 4.0;
+    public static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_NEVEREST * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);                   // Conversion: Encoder Count to Inches
+    public static final double COUNTS_PER_TETRIX_INCH = (COUNTS_PER_MOTOR_TETRIX * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);               // Conversion: Encoder Counts Motor Tetrix to Inches
+
+    public static final double FLICKER_CONVERSION = 1680/(2*COUNTS_PER_INCH);
+
 
 
 }
